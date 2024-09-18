2bd2d86a02dcbaebdc36eee24516b731ae3a7d9a
emiliaharden <emilia.harden@gmail.com>
2024-09-18 22:44:45 +0200

    Added password related functions and more

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -3,7 +3,9 @@ import {
   createUserController,
   deleteUserController,
   getUsersController,
+  resetUserPasswordController,
   updateUserController,
+  updateUserPasswordController,
 } from "../controllers/userController";
 
 const router = Router();
@@ -12,4 +14,10 @@ router.post("/user", createUserController);
 router.get("/users", getUsersController);
 router.put("/user/:id", updateUserController);
 router.delete("/user/:id", deleteUserController);
+
+//router för att uppdatera lösenordet
+router.put("/user/:id/update-password", updateUserPasswordController);
+// route för att reset lösenordet, måste ha token och email
+router.put("/user/:id/reset-password", resetUserPasswordController);
+
 export default router;
