79a61440e1525bd33ed1839d69c49cfd24a42d65
emilia <emilia.harden@gmail.com>
2024-09-21 00:57:01 +0200

    Reset password with token from email. User email got a link to reset the password. With nodemailer and gmail.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,14 @@
-import app from "./app";
 import dotenv from "dotenv";
-
 dotenv.config();
+
+import app from "./app";
+
+console.log("PORT:", process.env.PORT);
+console.log("EMAIL_USER:", process.env.EMAIL_USER);
+console.log("EMAIL_PASS:", process.env.EMAIL_PASS);
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running on port: ${port}`);
+  console.log(`Server running on port: ${port}`);
 });
