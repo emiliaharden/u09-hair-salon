02401b3baae05508f0879aa38850c849a13c4a4a
emilia <emilia.harden@gmail.com>
2024-09-24 20:16:34 +0200

    created register function

--- frontend/src/pages/register/registerPage.tsx
+++ frontend/src/pages/register/registerPage.tsx
@@ -1,6 +1,10 @@
+import { useState } from 'react'
 import FormComponent from '../../components/formComponents'
 
 const RegisterPage = () => {
+    const [error, setError] = useState<string | null>(null)
+    const [success, setSuccess] = useState<string | null>(null)
+
     const registerFields = [
         {
             label: 'Name',
@@ -23,18 +27,52 @@ const RegisterPage = () => {
         {
             label: 'Confirm password',
             type: 'password',
-            placeholder: 'Enter password',
-            name: 'password',
+            placeholder: 'Confirm password',
+            name: 'confirmPassword',
         },
     ]
+
+    const handleRegister = async (formData: { [key: string]: string }) => {
+        const { name, email, password, confirmPassword } = formData;
+
+        if (password !== confirmPassword) {
+            setError('Passwords do not match')
+            return;
+        }
+
+        try {
+            const response = await fetch('http://localhost:3000/api/auth/user', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ name, email, password, roles: 'user' })
+            })
+
+            const data = await response.json()
+            console.log(data)
+
+            if (response.ok) {
+                setSuccess('User registered successfully!')
+                setError(null)
+            } else {
+                setError(data.message || 'Error registering user');
+            }
+        } catch (error) {
+            console.error(error)
+            setError('Error registering user')
+        }
+    }
 
     return (
         <div>
             <h2>Register</h2>
+            {error && <p className='text-red-500'>{error}</p>}
+            {success && <p className='text-green-500'>{success}</p>}
             <FormComponent
                 fields={registerFields}
                 buttonText="Register"
-                onSubmit={(data) => console.log('Register form data:', data)}
+                onSubmit={handleRegister}
             />
         </div>
     )
