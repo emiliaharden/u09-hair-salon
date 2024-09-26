b4c950140ed48e56938f4de8ca2ce221afb27369
emilia <emilia.harden@gmail.com>
2024-09-26 20:15:56 +0200

    added create user on admin page and a checkbox for admins to apply roles on a new user

---
+++ frontend/src/pages/admin/createNewUser.tsx
@@ -0,0 +1,13 @@
+import CreateUserComponent from "../../components/CreateUserComponent"
+
+const CreateNewUser = () => {
+
+    return (
+        <>
+        <CreateUserComponent buttonText="Create user" />
+        </>
+    )
+}
+
+
+export default CreateNewUser
