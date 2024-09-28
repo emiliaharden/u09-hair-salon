811d66c60e12b37483216fc94ee3af37fa839954
emilia <emilia.harden@gmail.com>
2024-09-28 17:29:12 +0200

    Added shadcn and created table components

---
+++ frontend/src/components/table/TableRowComponent.tsx
@@ -0,0 +1,25 @@
+import { User } from '@/pages/admin/adminDashboardPage'
+import { TableCell, TableRow } from '../ui/table'
+import Actions from './Actions'
+
+interface TableRowProps {
+    rowData: User
+    columns: string[]
+    onEdit: (user: User) => void
+    onDelete: (userId: string) => void
+}
+
+const TableRowComponent: React.FC<TableRowProps> = ({ rowData, columns, onEdit, onDelete }) => {
+    return (
+        <TableRow>
+            {columns.map((column, index) => (
+                <TableCell key={index}>{rowData[column] || 'N/A'}</TableCell>
+            ))}
+            <TableCell>
+                <Actions rowData={rowData} onEdit={onEdit} onDelete={onDelete}></Actions>
+            </TableCell>
+        </TableRow>
+    )
+}
+
+export default TableRowComponent
