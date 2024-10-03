7730cb5471a77a86dab0952c7ab7ea55e19a7f21
emilia <emilia.harden@gmail.com>
2024-10-03 13:30:01 +0200

    added navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
 import { ModeToggle } from './components/ModeToogle'
+import Navbar from './components/Navbar'
 
 function App() {
     return (
         
         <main>
-          Hej app 
-          <ModeToggle />
+          <Navbar/>
+          {/* <ModeToggle /> */}
             <Outlet />
         </main>
   
