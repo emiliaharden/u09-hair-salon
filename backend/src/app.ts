1ba22dafdc59a13471f721399674ca117fd11519
emilia <emilia.harden@gmail.com>
2024-10-07 15:43:19 +0200

    Create booking function now works

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import cors from "cors";
 import express, { Express, Request, Response } from "express";
 import userRoutes from "./routes/userRoutes";
 import authRoutes from "./routes/authRoutes";
+import bookingRoutes from "./routes/bookingRoutes";
 import connectDB from "./config/db";
 
 const app: Express = express();
@@ -26,5 +27,7 @@ app.get("/", (req: Request, res: Response) => {
 app.use("/api", userRoutes);
 
 app.use("/api/auth", authRoutes);
+
+app.use("/api/bookings", bookingRoutes);
 
 export default app;
