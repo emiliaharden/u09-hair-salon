6395afdc6fa2712f33e281c680b10003a533dea7
emilia <emilia.harden@gmail.com>
2024-10-07 17:20:04 +0200

    Crud is now created for the bookings

--- backend/src/routes/bookingRoutes.ts
+++ backend/src/routes/bookingRoutes.ts
@@ -1,15 +1,49 @@
 import { Router } from "express";
 import { authMiddleware } from "../middleware/authMiddleware";
 import { roleMiddleware } from "../middleware/roleMiddleware";
-import { createBookingController } from "../controllers/BookingController";
+import {
+  createBookingController,
+  deleteBookingController,
+  getAllBookingsController,
+  getBookingByIdController,
+  updateBookingController,
+} from "../controllers/BookingController";
 
 const router = Router();
 
+router.put(
+  "/bookings/:id",
+  //   authMiddleware,
+  //   roleMiddleware("admin"),
+  updateBookingController
+);
+
+router.get(
+  "/bookings",
+  //   authMiddleware,
+  //   roleMiddleware("admin"),
+  getAllBookingsController
+);
+
+router.get(
+  "/bookings/:id",
+  //   authMiddleware,
+  //   roleMiddleware("admin"),
+  getBookingByIdController
+);
+
 router.post(
-  "/",
+  "/bookings",
   //   authMiddleware,
   //   roleMiddleware("user"),
   createBookingController
+);
+
+router.delete(
+  "/bookings/:id",
+  //   authMiddleware,
+  //   roleMiddleware("admin"),
+  deleteBookingController
 );
 
 export default router;
