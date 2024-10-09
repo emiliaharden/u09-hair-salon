8afa1e372876c9a77caa7dcdf75a461c6c66465a
emilia <emilia.harden@gmail.com>
2024-10-09 22:19:02 +0200

    Created a user navbar for the bookings.

---
+++ frontend/src/pages/dashboard/components/BookingHistory.tsx
@@ -0,0 +1,11 @@
+
+const BookingHistory = () => {
+    return (
+        <div>
+            <h2>Booking History</h2>
+            <p>Här var det tomt!</p>
+        </div>
+    )
+}
+
+export default BookingHistory;
