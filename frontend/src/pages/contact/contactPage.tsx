444964316930c33e1345dd1bb19964551fc03137
emilia <emilia.harden@gmail.com>
2024-10-19 23:58:52 +0200

    started with the styling on the user dashboard

--- frontend/src/pages/contact/contactPage.tsx
+++ frontend/src/pages/contact/contactPage.tsx
@@ -1,19 +1,27 @@
 const ContactPage = () => {
     return (
-        <div className="max-w-4xl mx-auto p-4">
-            <h2 className="text-2xl font-semibold mb-4">Contact Us</h2>
-            <p className="text-gray-700 mb-6">
-                If you have any questions or need assistance, feel free to reach out to us. We're
-                here to help!
+        <div className="p-4 text-center mt-10">
+            <h2 className="text-3xl font-bold mb-4">Kontakta oss</h2>
+            <hr className="mt-2 mb-4 border-t-2 border-gray-300 w-3/4 mx-auto" />
+
+            <p className="text-lg text-gray-600 w-3/4 mx-auto mb-6">
+                Har du några frågor eller behöver hjälp? Tveka inte att höra av dig till oss – vi finns här för att hjälpa dig!
             </p>
-            <p className="text-gray-700">
-                <strong>Email:</strong> info@yourcompany.com
-            </p>
-            <p className="text-gray-700">
-                <strong>Phone:</strong> 123-456-7890
-            </p>
+
+            <div className="w-3/4 mx-auto border border-gray-300 rounded-lg p-6 shadow-md">
+                <p className="text-gray-700 text-left mb-4">
+                    <strong>Email:</strong> info@salongsaxen.com
+                </p>
+                <p className="text-gray-700 text-left mb-4">
+                    <strong>Telefon:</strong> 08 - XXX XX XX
+                </p>
+                <p className="text-gray-700 text-left">
+                    <strong>Adress:</strong> Frisörgatan 1, 123 45 Stockholm
+                </p>
+            </div>
+
         </div>
     )
 }
 
-export default ContactPage
+export default ContactPage;
--- frontend/src/pages/dashboard/dashboardPage.tsx
+++ frontend/src/pages/dashboard/dashboardPage.tsx
@@ -3,10 +3,13 @@ import UserNavbar from './components/UserNavbar'
 
 const DashboardPage = () => {
     return (
-        <div>
-            <UserNavbar />
-            <div>
+        <div className="flex justify-between p-6 mt-10">
+            <div className="w-full max-w-4xl h-[600px] p-6 bg-white shadow-md rounded-lg overflow-y-auto">
                 <Outlet />
+            </div>
+
+            <div className="w-48 ml-4">
+                <UserNavbar />
             </div>
         </div>
     )
