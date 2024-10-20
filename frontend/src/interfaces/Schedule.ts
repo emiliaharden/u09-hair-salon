01de9770701bc6beaa277603d2941965825725a2
emilia <emilia.harden@gmail.com>
2024-10-20 19:16:36 +0200

    Added a bit in backend and frontend so an admin can now see which user that have a booking appoinment

--- frontend/src/interfaces/Schedule.ts
+++ frontend/src/interfaces/Schedule.ts
@@ -1,7 +1,10 @@
+import { Booking } from './Booking'
+
 export interface Slot {
     startTime: string
     endTime: string
     isBooked: boolean
+    booking?: Booking
 }
 
 export interface Schedule {
