92b4cbd4ca71f3d76c448728901581e2485ec53d
emiliaharden <emilia.harden@gmail.com>
2024-10-23 23:44:41 +0200

    Added delete function for schedules and styles

---
+++ backend/dist/src/app.js
@@ -0,0 +1,26 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const cors_1 = __importDefault(require("cors"));
+const express_1 = __importDefault(require("express"));
+const userRoutes_1 = __importDefault(require("./routes/userRoutes"));
+const authRoutes_1 = __importDefault(require("./routes/authRoutes"));
+const db_1 = __importDefault(require("./config/db"));
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+(0, db_1.default)();
+// logga inkommande requests för felsökning/testning
+app.use((req, res, next) => {
+    console.log(`Request received at ${req.path}`);
+    next();
+});
+// Routes
+app.get("/", (req, res) => {
+    res.send("Express + TypeScript Server");
+});
+app.use("/api", userRoutes_1.default);
+app.use("/api/auth", authRoutes_1.default);
+exports.default = app;
---
+++ backend/dist/src/services/AuthService.js
@@ -0,0 +1,46 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.registerUser = exports.loginUser = void 0;
+const UserModel_1 = __importDefault(require("../models/UserModel"));
+const bcrypt_1 = __importDefault(require("bcrypt"));
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const loginUser = async (email, password) => {
+    const user = await UserModel_1.default.findOne({ email });
+    if (!user)
+        throw new Error("User not found");
+    const isMatch = await bcrypt_1.default.compare(password, user.password);
+    if (!isMatch)
+        throw new Error("Invalid password");
+    //generera JWT-token
+    const token = jsonwebtoken_1.default.sign({ id: user.id, name: user.name, email: user.email, roles: user.roles }, process.env.JWT_SECRET || "secret", {
+        expiresIn: "1h",
+    });
+    return {
+        token,
+        user: { name: user.name, email: user.email, roles: user.roles },
+    };
+};
+exports.loginUser = loginUser;
+//här kan vi lägga till andra auth operations, som logout eller change of password
+const registerUser = async (name, email, password, roles) => {
+    const rolesArray = Array.isArray(roles) ? roles : [roles];
+    const salt = await bcrypt_1.default.genSalt(10);
+    const hashedPassword = await bcrypt_1.default.hash(password, salt);
+    const newUser = new UserModel_1.default({
+        name,
+        email,
+        password: hashedPassword,
+        roles: rolesArray,
+    });
+    try {
+        return await newUser.save();
+    }
+    catch (error) {
+        console.error("Error registering user:", error);
+        throw new Error("User registration failed");
+    }
+};
+exports.registerUser = registerUser;
